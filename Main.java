93a3882e672bef5881325e23c94615a34b7c0c2a
Modo <yashlaxman@gmail.com>
2023-11-11 22:02:07 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,193 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.*;
+
+public class Main{
+    public static void main(String[] args) {
+        launch();
+    }
+
+    static JFrame myFrame = new JFrame("Morse Converter");
+
+    public static void launch() {
+        String[] opts = {"Morse->Text","Text->Morse"};
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+        JPanel p3 = new JPanel();
+        JLabel title = new JLabel("Morse Converter");
+        JComboBox<String> optbox = new JComboBox<>(opts);
+        JTextArea field = new JTextArea();
+        JButton btn = new JButton("Convert");
+        JScrollPane sp = new JScrollPane(field);
+        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        myFrame.setSize(500,300);
+        myFrame.setLayout(new BorderLayout());
+        p1.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        p2.setLayout(new BorderLayout());
+        p3.setLayout(new BorderLayout());
+        p3.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        optbox.setBounds(0,0,100,20);
+        optbox.setBackground(Color.WHITE);
+        field.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        btn.setFocusable(false);
+        field.setLineWrap(true);
+        field.setWrapStyleWord(true);
+        field.setFont(new Font("Courier New", Font.BOLD, 14));
+        p1.add(title);
+        p1.add(optbox);
+        p1.add(btn);
+        p3.add(sp, BorderLayout.CENTER);
+        p2.add(p3, BorderLayout.CENTER);
+        p1.setBackground(Color.LIGHT_GRAY);
+        myFrame.add(p1, BorderLayout.NORTH);
+        myFrame.add(p2, BorderLayout.CENTER);
+        myFrame.setResizable(false);
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setVisible(true);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(optbox.getSelectedIndex()==0){
+                    String[] argsArray = field.getText().replace("\n", "  ").split(" ");
+                    String output = "";
+                    for(String args : argsArray){
+                        output = output.concat(toText(args));
+                    }
+                    System.out.println(output);
+                    display(output);
+                } 
+                else if(optbox.getSelectedIndex()==1){
+                    char[] charArray = field.getText().toUpperCase().replace("\n", " ").toCharArray();
+                    String output = "";
+                    for(char c : charArray){
+                        output = output.concat(getMorse(c));
+                    }
+                    System.out.println(output);
+                    display(output);
+                }
+            }
+        });
+        optbox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                field.setText("");
+            }
+        });
+    }
+
+    public static String toText(String query) {
+        Map<Character, String> morseCodeMap = globalMap();
+        Map<String, Character> inverseMap = new HashMap<>();
+        for(Map.Entry<Character, String> entry : morseCodeMap.entrySet()){
+            inverseMap.put(entry.getValue(), entry.getKey());
+        }
+        if(inverseMap.get(query)==null){
+            return "?";
+        }
+        else{
+            return inverseMap.get(query).toString();
+        }
+    }
+
+    public static String getMorse(Character query) {
+        Map<Character, String> morseCodeMap = globalMap();
+        if(morseCodeMap.get(query)==null){
+            return "x";
+        }
+        else{
+            return morseCodeMap.get(query).toString().concat(" ");
+        }
+
+    }
+
+    public static Map<Character, String> globalMap() {
+        Map<Character, String> morseCodeMap = new HashMap<>();
+            morseCodeMap.put('A', ".-");
+            morseCodeMap.put('B', "-...");
+            morseCodeMap.put('C', "-.-.");
+            morseCodeMap.put('D', "-..");
+            morseCodeMap.put('E', ".");
+            morseCodeMap.put('F', "..-.");
+            morseCodeMap.put('G', "--.");
+            morseCodeMap.put('H', "....");
+            morseCodeMap.put('I', "..");
+            morseCodeMap.put('J', ".---");
+            morseCodeMap.put('K', "-.-");
+            morseCodeMap.put('L', ".-..");
+            morseCodeMap.put('M', "--");
+            morseCodeMap.put('N', "-.");
+            morseCodeMap.put('O', "---");
+            morseCodeMap.put('P', ".--.");
+            morseCodeMap.put('Q', "--.-");
+            morseCodeMap.put('R', ".-.");
+            morseCodeMap.put('S', "...");
+            morseCodeMap.put('T', "-");
+            morseCodeMap.put('U', "..-");
+            morseCodeMap.put('V', "...-");
+            morseCodeMap.put('W', ".--");
+            morseCodeMap.put('X', "-..-");
+            morseCodeMap.put('Y', "-.--");
+            morseCodeMap.put('Z', "--..");
+            morseCodeMap.put(' ', "/");
+            morseCodeMap.put('.', ".-.-.-");
+            morseCodeMap.put(',', "--..--");
+            morseCodeMap.put('?', "..--..");
+            morseCodeMap.put('!', "-.-.--");
+            morseCodeMap.put('&', ".-...");  
+            morseCodeMap.put('\'', ".----."); 
+            morseCodeMap.put('@', ".--.-."); 
+            morseCodeMap.put(':', "---..."); 
+            morseCodeMap.put('-', "-....-");
+            morseCodeMap.put('(', "-.--."); 
+            morseCodeMap.put(')', "-.--.-");
+            morseCodeMap.put('"', ".-..-.");
+            morseCodeMap.put('0', "-----");
+            morseCodeMap.put('1', ".----");
+            morseCodeMap.put('2', "..---");
+            morseCodeMap.put('3', "...--");
+            morseCodeMap.put('4', "....-");
+            morseCodeMap.put('5', ".....");
+            morseCodeMap.put('6', "-....");
+            morseCodeMap.put('7', "--...");
+            morseCodeMap.put('8', "---..");
+            morseCodeMap.put('9', "----.");
+        return morseCodeMap;
+    }
+
+    public static void display(String disptext){
+        myFrame.setVisible(false);
+        JFrame df = new JFrame("Morse Converter");
+        JPanel p1 = new JPanel();
+        JTextArea disp = new JTextArea();
+        JScrollPane sp = new JScrollPane(disp);
+        df.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        disp.setText(disptext);
+        disp.setEditable(false);
+        disp.setLineWrap(true);
+        disp.setWrapStyleWord(true);
+        disp.setFont(new Font("Courier New", Font.BOLD, 14));
+        df.setSize(600,400);
+        p1.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        p1.setLayout(new BorderLayout());
+        df.add(p1);
+        p1.add(sp, BorderLayout.CENTER);
+        df.setResizable(false);
+        df.setLocationRelativeTo(null);
+        df.setVisible(true);
+        df.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                myFrame.setVisible(true);
+            }
+        });
+    }
+
+}
